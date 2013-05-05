f8bd0f729d71a0f81753f6c6e3a2f2cadea59f41
striversist <striversist@qq.com>
2013-05-05 23:14:29 +0800

    1. 增加ChannellistActivity显示logo的功能，初步 2. 准备重构：channels.xml，将本地channel相关的信息放入xml中统一管理

--- src/com/tools/tvguide/activities/ChannellistActivity.java
+++ src/com/tools/tvguide/activities/ChannellistActivity.java
@@ -1,7 +1,10 @@
 package com.tools.tvguide.activities;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -15,18 +18,24 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Pair;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleAdapter.ViewBinder;
 
 public class ChannellistActivity extends Activity 
 {
     private String mCategoryId;
     private ListView mChannelListView;
     private List<Pair<String, String>> mChannelList;
+    private SimpleAdapter mListViewAdapter;
+    private ArrayList<HashMap<String, Object>> mItemList;
     private HandlerThread mUpdateThread;
     private Handler mUpdateHandler;
     @Override
@@ -36,7 +45,9 @@ public class ChannellistActivity extends Activity
         setContentView(R.layout.activity_channellist);
         mChannelListView = (ListView)findViewById(R.id.channel_list);
         mChannelList = new ArrayList<Pair<String,String>>();
+        mItemList = new ArrayList<HashMap<String, Object>>();
         createUpdateThreadAndHandler();
+        createAndSetListViewAdapter();
         
         mCategoryId = getIntent().getStringExtra("category");
         if (mCategoryId != null)
@@ -69,6 +80,31 @@ public class ChannellistActivity extends Activity
         mUpdateHandler = new Handler(mUpdateThread.getLooper());
     }
     
+    private void createAndSetListViewAdapter()
+    {
+        mListViewAdapter = new SimpleAdapter(ChannellistActivity.this, mItemList, R.layout.channellist_item,
+                new String[]{"image", "name"}, 
+                new int[]{R.id.itemImage, R.id.itemText});
+        mListViewAdapter.setViewBinder(new MyViewBinder());
+        mChannelListView.setAdapter(mListViewAdapter);
+    }
+    
+    public class MyViewBinder implements ViewBinder
+    {
+        public boolean setViewValue(View view, Object data,
+                String textRepresentation)
+        {
+            if((view instanceof ImageView) && (data instanceof Bitmap))
+            {
+                ImageView iv = (ImageView)view;
+                Bitmap bm = (Bitmap)data;
+                iv.setImageBitmap(bm);
+                return true;
+            }
+            return false;
+        }
+    }
+       
     private void update()
     {
         mUpdateHandler.post(new Runnable()
@@ -109,6 +145,24 @@ public class ChannellistActivity extends Activity
         });
     }
     
+    private Bitmap getImage(String fileName)
+    {
+        Bitmap bitmap = null;
+        try
+        {
+            InputStream is = getAssets().open(fileName);
+            bitmap = BitmapFactory.decodeStream(is);
+            is.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        
+        return bitmap;
+    }    
+    
     private Handler uiHandler = new Handler()
     {
         public void handleMessage(Message msg)
@@ -116,12 +170,14 @@ public class ChannellistActivity extends Activity
             super.handleMessage(msg);
             if (mChannelList != null)
             {
-                String channelList[] = new String[mChannelList.size()];
-                for (int i=0; i<mChannelList.size(); ++i)
+                for(int i=0; i<mChannelList.size(); ++i)
                 {
-                    channelList[i] = mChannelList.get(i).second;
+                    HashMap<String, Object> item = new HashMap<String, Object>();
+                    item.put("image", getImage(mChannelList.get(i).first + ".jpg"));
+                    item.put("name", mChannelList.get(i).second);
+                    mItemList.add(item);
                 }
-                mChannelListView.setAdapter(new ArrayAdapter<String>(ChannellistActivity.this, android.R.layout.simple_list_item_1, channelList));
+                mListViewAdapter.notifyDataSetChanged();
             }
         }
     };
