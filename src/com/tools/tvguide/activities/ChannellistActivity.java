07045c0ca284fdb1f0075dd54e5beafb5415eda0
striversist <striversist@qq.com>
2013-05-06 22:47:35 +0800

    1. 完成对channels.xml解析 2. 部分完成ChannellistActivity的logo添加显示

--- src/com/tools/tvguide/activities/ChannellistActivity.java
+++ src/com/tools/tvguide/activities/ChannellistActivity.java
@@ -7,9 +7,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import com.tools.tvguide.utils.NetDataGetter;
 
@@ -34,20 +42,27 @@ public class ChannellistActivity extends Activity
     private String mCategoryId;
     private ListView mChannelListView;
     private List<Pair<String, String>> mChannelList;
+    private HashMap<String, HashMap<String, Object>> mXmlChannelInfo;
     private SimpleAdapter mListViewAdapter;
     private ArrayList<HashMap<String, Object>> mItemList;
     private HandlerThread mUpdateThread;
     private Handler mUpdateHandler;
+    private final String XML_FILE = "channels.xml";
+    private final String XML_ELEMENT_LOGO = "logo";
+    private final String XML_ELEMENT_CHANNEL = "channel";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channellist);
         mChannelListView = (ListView)findViewById(R.id.channel_list);
-        mChannelList = new ArrayList<Pair<String,String>>();
+        mChannelList = new ArrayList<Pair<String, String>>();
+        mXmlChannelInfo = new HashMap<String, HashMap<String,Object>>();
         mItemList = new ArrayList<HashMap<String, Object>>();
         createUpdateThreadAndHandler();
         createAndSetListViewAdapter();
+        parseXML();
         
         mCategoryId = getIntent().getStringExtra("category");
         if (mCategoryId != null)
@@ -132,11 +147,11 @@ public class ChannellistActivity extends Activity
                         }
                     }
                     uiHandler.sendEmptyMessage(0);
-                } 
+                }
                 catch (MalformedURLException e) 
                 {
                     e.printStackTrace();
-                } 
+                }
                 catch (JSONException e) 
                 {
                     e.printStackTrace();
@@ -161,7 +176,59 @@ public class ChannellistActivity extends Activity
         }
         
         return bitmap;
-    }    
+    }
+    
+    private void parseXML()
+    {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try 
+        {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(getAssets().open(XML_FILE));
+            NodeList nl1 = doc.getElementsByTagName(XML_ELEMENT_CHANNEL);
+            for (int i=0; i<nl1.getLength(); ++i) 
+            {
+                String id = null, logo = null;
+                Node n = nl1.item(i);
+                if (n.hasAttributes())
+                {
+                    id = n.getAttributes().getNamedItem("id").getNodeValue();
+                }
+                // 获取 n 节点下所有的子节点。此处值得注意，在DOM解析时会将所有回车都视为 n 节点的子节点。
+                NodeList nl2 = n.getChildNodes();
+                for (int j=0; j<nl2.getLength(); ++j) 
+                {
+                    Node n2 = nl2.item(j);
+                    if (n2.hasChildNodes()) 
+                    {
+                        String name = n2.getNodeName();
+                        if (name.equalsIgnoreCase(XML_ELEMENT_LOGO))
+                        {
+                            logo = n2.getFirstChild().getNodeValue();
+                        }
+                    }
+                }
+                if (id != null && logo != null)
+                {
+                    HashMap<String, Object> item = new HashMap<String, Object>();
+                    item.put(XML_ELEMENT_LOGO, logo);
+                    mXmlChannelInfo.put(id, item);
+                }
+            }
+        } 
+        catch (ParserConfigurationException e) 
+        {
+            e.printStackTrace();
+        } 
+        catch (SAXException e) 
+        {
+            e.printStackTrace();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+    }
     
     private Handler uiHandler = new Handler()
     {
@@ -173,7 +240,10 @@ public class ChannellistActivity extends Activity
                 for(int i=0; i<mChannelList.size(); ++i)
                 {
                     HashMap<String, Object> item = new HashMap<String, Object>();
-                    item.put("image", getImage(mChannelList.get(i).first + ".jpg"));
+                    if (mXmlChannelInfo.get(mChannelList.get(i).first) != null)
+                    {
+                        item.put("image", getImage((String) mXmlChannelInfo.get(mChannelList.get(i).first).get(XML_ELEMENT_LOGO)));                        
+                    }
                     item.put("name", mChannelList.get(i).second);
                     mItemList.add(item);
                 }
