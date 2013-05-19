7cce531a04901c380b7d3be4c9c485af070bbc55
striversist <striversist@qq.com>
2013-05-19 23:13:47 +0800

    1. 初步完成搜索结果展示 2. 搜索完毕之后自动隐藏软键盘

--- src/com/tools/tvguide/activities/SearchActivity.java
+++ src/com/tools/tvguide/activities/SearchActivity.java
@@ -1,25 +1,40 @@
 package com.tools.tvguide.activities;
 
+import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.tools.tvguide.utils.NetDataGetter;
+import com.tools.tvguide.utils.NetworkManager;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SearchActivity extends Activity 
 {
     private AutoCompleteTextView mSearchTextView;
     private ListView mListView;
+    private BaseAdapter mListViewAdapter;
     private ArrayList<ListItems> mItemList;
     private LayoutInflater mInflater;
+    private Handler mUpdateHandler;
     
     class PartAdapter extends BaseAdapter 
     {
@@ -60,21 +75,25 @@ public class SearchActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         
-        // For test
-        LabelItem label = new LabelItem("CCTV-1");
-        mItemList.add(label);
-        Item item = new Item();
-        item.id = "cctv1";
-        item.name = "CCTV-1";
-        item.time = "00:26";
-        item.title = "正大综艺";
-        ContentItem contentItem = new ContentItem(item);
-        mItemList.add(contentItem);
-        
         mSearchTextView = (AutoCompleteTextView)findViewById(R.id.search_auto_text_view);
         mListView = (ListView)findViewById(R.id.search_list_view);
         mItemList = new ArrayList<ListItems>();
-        mListView.setAdapter(new PartAdapter());
+        mListViewAdapter = new PartAdapter();
+        mListView.setAdapter(mListViewAdapter);
+        mInflater = LayoutInflater.from(this);
+        createUpdateThreadAndHandler();
+        
+        // For test
+//        LabelItem label = new LabelItem("CCTV-1");
+//        mItemList.add(label);
+//        Item item = new Item();
+//        item.id = "cctv1";
+//        item.name = "CCTV-1";
+//        item.time = "00:26";
+//        item.title = "正大综艺";
+//        ContentItem contentItem = new ContentItem(item);
+//        mItemList.add(contentItem);
+//        mListViewAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -84,11 +103,94 @@ public class SearchActivity extends Activity
         getMenuInflater().inflate(R.menu.activity_search, menu);
         return true;
     }
+
+    private void createUpdateThreadAndHandler()
+    {
+        mUpdateHandler = new Handler(NetworkManager.getInstance().getNetworkThreadLooper());
+    }
     
     public void search(View view)
     {
+        if (mSearchTextView.getText().toString().trim().equals(""))
+        {
+            Toast.makeText(this, "请输入要搜索的节目关键字!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        updateResult();
     }
-
+    
+    private void updateResult()
+    {
+        mUpdateHandler.post(new Runnable()
+        {
+            public void run()
+            {
+                String url = "http://192.168.1.103/projects/TV/json/search.php?" + "keyword=" + mSearchTextView.getText().toString();
+                NetDataGetter getter;
+                try 
+                {
+                    getter = new NetDataGetter(url);
+                    JSONObject jsonRoot = getter.getJSONsObject();
+                    mItemList.clear();
+                    if (jsonRoot != null)
+                    {
+                        JSONArray resultArray = jsonRoot.getJSONArray("result");
+                        if (resultArray != null)
+                        {
+                            
+                            for (int i=0; i<resultArray.length(); ++i)
+                            {
+                                String id = resultArray.getJSONObject(i).getString("id");
+                                String name = resultArray.getJSONObject(i).getString("name");
+                                JSONArray programsArray = resultArray.getJSONObject(i).getJSONArray("programs");
+                                
+                                mItemList.add(new LabelItem(name));
+                                if (programsArray != null)
+                                {
+                                    for (int j=0; j<programsArray.length(); ++j)
+                                    {
+                                        String time = programsArray.getJSONObject(j).getString("time");
+                                        String title = programsArray.getJSONObject(j).getString("title");                               
+                                        Item item = new Item();
+                                        item.id = id;
+                                        item.name = name;
+                                        item.time = time;
+                                        item.title = title;
+                                        mItemList.add(new ContentItem(item));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    uiHandler.sendEmptyMessage(0);
+                }
+                catch (MalformedURLException e) 
+                {
+                    e.printStackTrace();
+                }
+                catch (JSONException e) 
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    
+    private void hideInputKeyboard()
+    {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mSearchTextView.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+    
+    private Handler uiHandler = new Handler()
+    {
+        public void handleMessage(Message msg)
+        {
+            super.handleMessage(msg);
+            hideInputKeyboard();
+            mListViewAdapter.notifyDataSetChanged();
+        }
+    };
 }
 
 interface ListItems
@@ -109,7 +211,7 @@ class LabelItem implements ListItems
     @Override
     public int getLayout() 
     {
-        return android.R.layout.simple_list_item_1;
+        return R.layout.search_list_label_item;
     }
 
     @Override
@@ -122,7 +224,7 @@ class LabelItem implements ListItems
     public View getView(Context context, View convertView, LayoutInflater inflater) 
     {
         convertView = inflater.inflate(getLayout(), null);
-        TextView title = (TextView) convertView;
+        TextView title = (TextView) convertView.findViewById(R.id.search_item_label_text_view);
         title.setText(mLabel);
         return convertView;
     }
@@ -148,7 +250,7 @@ class ContentItem implements ListItems
     @Override
     public int getLayout() 
     {
-        return android.R.layout.simple_list_item_2;
+        return android.R.layout.simple_list_item_1;
     }
 
     @Override
