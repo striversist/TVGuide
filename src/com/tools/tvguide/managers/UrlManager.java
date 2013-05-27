4cfd205a0a621cf164b40664a9ebc346a5823490
striversist <striversist@qq.com>
2013-05-27 23:21:06 +0800

    UrlManager.java的地址部分的重构

--- src/com/tools/tvguide/managers/UrlManager.java
+++ src/com/tools/tvguide/managers/UrlManager.java
@@ -4,13 +4,14 @@ import android.content.Context;
 
 public class UrlManager 
 {
-    public static final String TAG = "UrlManager";
-    public static final String URL_CATEGORIES = "http://192.168.1.103/projects/TV/json/categories.php";
-    public static final String URL_CHANNELS = "http://192.168.1.103/projects/TV/json/channels.php";
-    public static final String URL_ON_PLAYING_PROGRAM = "http://192.168.1.103/projects/TV/json/onplaying_program.php";
-    public static final String URL_ON_PLAYING_PROGRAMS = "http://192.168.1.103/projects/TV/json/onplaying_programs.php";
-    public static final String URL_CHOOSE = "http://192.168.1.103/projects/TV/json/choose.php";
-    public static final String URL_SEARCH = "http://192.168.1.103/projects/TV/json/search.php";
+    public static final String TAG                      = "UrlManager";
+    private static final String HOST                    = "192.168.1.101";
+    public static final String URL_CATEGORIES           = "http://" + HOST + "/projects/TV/json/categories.php";
+    public static final String URL_CHANNELS             = "http://" + HOST + "/projects/TV/json/channels.php";
+    public static final String URL_ON_PLAYING_PROGRAM   = "http://" + HOST + "/projects/TV/json/onplaying_program.php";
+    public static final String URL_ON_PLAYING_PROGRAMS  = "http://" + HOST + "/projects/TV/json/onplaying_programs.php";
+    public static final String URL_CHOOSE               = "http://" + HOST + "/projects/TV/json/choose.php";
+    public static final String URL_SEARCH               = "http://" + HOST + "/projects/TV/json/search.php";
     
     private Context mContext;
     
