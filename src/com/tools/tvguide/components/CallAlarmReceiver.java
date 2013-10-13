01654580573619aedbbda6c9a68bdd8c0b057825
striversist <striversist@qq.com>
2013-10-13 18:00:00 +0800

    【功能】处理BOOT_COMPLETED广播消息（尽管android3.1之后，stopped状态的程序收不到该广播了）

--- src/com/tools/tvguide/components/CallAlarmReceiver.java
+++ src/com/tools/tvguide/components/CallAlarmReceiver.java
@@ -1,6 +1,7 @@
 package com.tools.tvguide.components;
 
 import com.tools.tvguide.activities.AlarmAlertActivity;
+import com.tools.tvguide.managers.AppEngine;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -10,20 +11,31 @@ import android.util.Log;
 public class CallAlarmReceiver extends BroadcastReceiver 
 {
     private static final String TAG = "CallAlarmReceiver";
+    private Context mContext;
     
     @Override
     public void onReceive(Context context, Intent intent) 
     {
 //        Log.d(TAG, "onReceive: action=" + intent.getAction());
+        mContext = context;
         String action = intent.getAction();
         if (action != null && action.equals(Intent.ACTION_BOOT_COMPLETED)) 
         {
 //            Log.d(TAG, "onReceive boot completed");
+            resetAllAlarms();
             return;
         }
         
         intent.setClass(context, AlarmAlertActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
+    }
+    
+    // 重新计算闹铃时间
+    private void resetAllAlarms()
+    {
+        if (AppEngine.getInstance().getContext() == null)
+            AppEngine.getInstance().setContext(mContext);
+        AppEngine.getInstance().getAlarmHelper().resetAllAlarms();
     }
 }
