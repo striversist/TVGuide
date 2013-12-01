639b8c17d25a4860b5f4ebcf4be442caf0ecfd10
striversist <striversist@qq.com>
2013-12-01 21:17:53 +0800

    【功能】节目详细页面，增加分类Tabs，初步展示“演员表”和“剧情简介”中的内容

--- src/com/tools/tvguide/ProgramActivity.java
+++ src/com/tools/tvguide/ProgramActivity.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
+import com.tools.tvguide.adapters.ResultPageAdapter;
 import com.tools.tvguide.managers.AppEngine;
 import com.tools.tvguide.managers.HotHtmlManager.ProgramDetailCallback;
 import com.tools.tvguide.utils.NetDataGetter;
@@ -11,10 +12,14 @@ import com.tools.tvguide.utils.NetDataGetter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class ProgramActivity extends Activity 
@@ -31,12 +36,21 @@ public class ProgramActivity extends Activity
     private TextView mProgramNameTextView;
     private TextView mProgramProfileTextView;
     private ImageView mProgramImageView;
+    private ViewPager mViewPager;
+    private ResultPageAdapter mProgramPageAdapter;
+    private TextView mActorsTextView;
+    private TextView mSummaryTextView;
+    private TextView mPlayTimesTextView;
     
     private final int MSG_PROFILE_LOADED = 1;
     private final int MSG_SUMMARY_LOADED = 2;
     private final int MSG_PICTURE_LOADED = 3;
     private final int MSG_ACTORS_LOADED = 4;
     private final int MSG_PLAYTIMES_LOADED = 5;
+    
+    private final int TAB_INDEX_ACTORS = 0;
+    private final int TAB_INDEX_SUMMARY = 1;
+    private final int TAB_INDEX_PLAYTIMES = 2;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) 
@@ -46,6 +60,41 @@ public class ProgramActivity extends Activity
         mProgramNameTextView = (TextView) findViewById(R.id.program_name);
         mProgramProfileTextView = (TextView) findViewById(R.id.program_profile);
         mProgramImageView = (ImageView) findViewById(R.id.program_image);
+        mViewPager = (ViewPager) findViewById(R.id.program_view_pager);
+        
+        mProgramPageAdapter = new ResultPageAdapter();
+        mActorsTextView = new TextView(this);
+        mSummaryTextView = new TextView(this);
+        mPlayTimesTextView = new TextView(this);
+        mProgramPageAdapter.addView(mActorsTextView);
+        mProgramPageAdapter.addView(mSummaryTextView);
+        mProgramPageAdapter.addView(mPlayTimesTextView);
+        mViewPager.setAdapter(mProgramPageAdapter);
+        
+        mViewPager.setOnPageChangeListener(new OnPageChangeListener() 
+        {
+            @Override
+            public void onPageSelected(int position) 
+            {
+                RadioGroup radioTabs = (RadioGroup) findViewById(R.id.program_tabs);
+                if (position == TAB_INDEX_ACTORS)
+                    radioTabs.check(R.id.program_tab_actors);
+                else if (position == TAB_INDEX_SUMMARY)
+                    radioTabs.check(R.id.program_tab_summary);
+                else if (position == TAB_INDEX_PLAYTIMES)
+                    radioTabs.check(R.id.program_tab_playtimes);
+            }
+            
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) 
+            {
+            }
+            
+            @Override
+            public void onPageScrollStateChanged(int state) 
+            {
+            }
+        });
         
         mName = getIntent().getStringExtra("name");
         mLink = getIntent().getStringExtra("link");
@@ -61,6 +110,22 @@ public class ProgramActivity extends Activity
     public void onBackPressed() 
     {
         finish();
+    }
+    
+    public void onClickTabs(View view)
+    {
+        switch(view.getId())
+        {
+            case R.id.program_tab_actors:
+                mViewPager.setCurrentItem(TAB_INDEX_ACTORS);
+                break;
+            case R.id.program_tab_summary:
+                mViewPager.setCurrentItem(TAB_INDEX_SUMMARY);
+                break;
+            case R.id.program_tab_playtimes:
+                mViewPager.setCurrentItem(TAB_INDEX_PLAYTIMES);
+                break;
+        }
     }
     
     private void update()
@@ -142,8 +207,10 @@ public class ProgramActivity extends Activity
                     mProgramImageView.setImageBitmap(mPicture);
                     break;
                 case MSG_SUMMARY_LOADED:
+                    mSummaryTextView.setText(mSummary);
                     break;
                 case MSG_ACTORS_LOADED:
+                    mActorsTextView.setText(mActors);
                     break;
                 case MSG_PLAYTIMES_LOADED:
                     break;
