7cff4d7ee5b8fa960a7ef4a3feec6610ae5a4a72
aaron <striversist@qq.com>
2013-12-26 19:31:43 +0800

    【功能】添加ACRA，初步

--- src/com/tools/tvguide/utils/MyApplication.java
+++ src/com/tools/tvguide/utils/MyApplication.java
@@ -1,6 +1,21 @@
 package com.tools.tvguide.utils;
 
+import com.tools.tvguide.R;
+
+import org.acra.ACRA;
+import org.acra.ErrorReporter;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
 import android.app.Application;
+
+@ReportsCrashes(formKey="",
+	mailTo = "bigeyecow@qq.com",
+	customReportContent = { ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT },
+	mode = ReportingInteractionMode.TOAST,
+	forceCloseDialogAfterToast = false, // optional, default false
+	resToastText = R.string.crash_toast_text)
 
 public class MyApplication extends Application
 {
@@ -17,5 +32,7 @@ public class MyApplication extends Application
 	{
 		super.onCreate();
 		sInstance = this;
+		
+		ACRA.init(this);
 	}
 }
