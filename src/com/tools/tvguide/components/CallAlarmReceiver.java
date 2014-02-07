66e4f84919a35e4b518149b081d8491a60677624
striversist <striversist@qq.com>
2014-02-07 15:16:13 +0800

    【功能】新增BootCompleteReceiver，在其中打开监听器和重设闹钟（验证OK）

--- src/com/tools/tvguide/components/CallAlarmReceiver.java
+++ src/com/tools/tvguide/components/CallAlarmReceiver.java
@@ -1,12 +1,10 @@
 package com.tools.tvguide.components;
 
 import com.tools.tvguide.activities.AlarmAlertActivity;
-import com.tools.tvguide.managers.AppEngine;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class CallAlarmReceiver extends BroadcastReceiver 
 {
@@ -16,26 +14,10 @@ public class CallAlarmReceiver extends BroadcastReceiver
     @Override
     public void onReceive(Context context, Intent intent) 
     {
-//        Log.d(TAG, "onReceive: action=" + intent.getAction());
         mContext = context;
-        String action = intent.getAction();
-        if (action != null && action.equals(Intent.ACTION_BOOT_COMPLETED)) 
-        {
-//            Log.d(TAG, "onReceive boot completed");
-            resetAllAlarms();
-            return;
-        }
         
         intent.setClass(context, AlarmAlertActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
-    }
-    
-    // 重新计算闹铃时间
-    private void resetAllAlarms()
-    {
-        if (AppEngine.getInstance().getContext() == null)
-            AppEngine.getInstance().setContext(mContext);
-        AppEngine.getInstance().getAlarmHelper().resetAllAlarms();
     }
 }
