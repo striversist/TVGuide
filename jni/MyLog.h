5bf3e76ab20418859eff74552cc9229002733940
aaron <striversist@qq.com>
2014-02-08 11:03:37 +0800

    【功能】增加XLOG开关控制宏

--- jni/MyLog.h
+++ jni/MyLog.h
@@ -1,5 +1,13 @@
 #include <android/log.h>
 
+#define DEBUG_MOD       1
+
+#if DEBUG_MOD
 #define XLOG(FORMAT,...) \
     { __android_log_print(ANDROID_LOG_DEBUG, "XLOG", FORMAT, ##__VA_ARGS__); }
+
+#else
+#define XLOG(FORMAT,...) 
+
+#endif
 
