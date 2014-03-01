5ea62a942a2ac9579cdfe2c0ca78fb4da6346cdf
striversist <striversist@qq.com>
2014-03-01 11:10:05 +0800

    【广告】添加Google Admob

--- src/com/tools/tvguide/managers/AdManager.java
+++ src/com/tools/tvguide/managers/AdManager.java
@@ -1,5 +1,8 @@
 package com.tools.tvguide.managers;
 
+import com.google.ads.AdRequest;
+import com.google.ads.AdView;
+
 import android.app.Activity;
 import android.content.Context;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.widget.RelativeLayout;
 public class AdManager 
 {
 	public static final String TAG = "AdManager";
+	private static final String ADMOB_ID = "a15306e4c0310af";
 	public enum AdSize {MINI_SIZE, NORMAL_SIZE};
 	
 	private Context mContext;
@@ -41,8 +45,14 @@ public class AdManager
 		switch (size) 
 		{
 			case MINI_SIZE:
+			    AdView adViewMini = new AdView(activity, com.google.ads.AdSize.SMART_BANNER, ADMOB_ID);
+			    layout.addView(adViewMini);
+			    adViewMini.loadAd(new AdRequest());
 				break;
 			case NORMAL_SIZE:
+			    AdView adView = new AdView(activity, com.google.ads.AdSize.BANNER, ADMOB_ID);
+                layout.addView(adView);
+                adView.loadAd(new AdRequest());
 				break;
 		}
 		layout.setVisibility(View.VISIBLE);
