5890c87d903d09d3ab01fadfc755e64caf17c2cf
aarontang <aarontang@tencent.com>
2015-11-02 16:50:45 +0800

    【More】调整版权信息

--- src/com/tools/tvguide/activities/MoreActivity.java
+++ src/com/tools/tvguide/activities/MoreActivity.java
@@ -158,7 +158,7 @@ public class MoreActivity extends Activity implements Callback
         AlertDialog dialog = new AlertDialog.Builder(MoreActivity.this)
             .setIcon(R.drawable.application)
             .setTitle(getResources().getString(R.string.app_name))
-            .setMessage("作者：大眼牛工作室\n邮箱：bigeyecow@qq.com\n版权：©2013-2014")
+            .setMessage("作者：大眼牛工作室\n邮箱：bigeyecow@qq.com\n版权：©2015-2016")
             .setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() 
             {
                 @Override
